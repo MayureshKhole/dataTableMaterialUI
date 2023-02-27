62a2fcc6a71eb393e4d8113f9c7bac4f7f2c1f47
MayureshKhole <107555160+MayureshKhole@users.noreply.github.com>
2023-02-27 15:55:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import "./App.css";
+import DataTableTest from "./DataTableTest";
+function App() {
+  return <DataTableTest></DataTableTest>;
+}
+
+export default App;
